de20b5914299e8409e8bc55283622ece42629ffc
ioanassenov <ioancho@umich.edu>
2025-11-25 14:19:30 -0500

    Initial commit

---
+++ dataprocessing.m
@@ -0,0 +1,29 @@
+clear; clc; close all;
+
+% Load data
+data = load("MEMS32_DownSweep_To_Fixed_Test_2.mat");
+
+% Extract relevant fields from the loaded data
+t = data.data.Time;
+Vin = data.data.V_in;
+Vldv = data.data.V_LDV;
+
+% Useful properties from data:
+T = seconds(0.00025);    % Period of input
+offset = seconds(1e-5);  % offset from start of first period in graph
+SStime = seconds(0.2424);% Time of first steady state peak of repsonse
+T_sys = seconds(0.03321);% Period of system
+f_sys = 1/0.03321;
+
+% Plotting
+startT = 4990; % Start input period to plot from (exclusive)
+endT = 5010;   % Last input period to plot until (inclusive)
+hold on;
+yyaxis left;
+plot(t, Vin, "LineWidth", 2);
+yyaxis right;
+plot(t, Vldv, "LineWidth", 2);
+xlim([offset+startT*T, endT*T]);
+% xlim([SStime+10*T_sys, 100*T_sys]);
+% xlim([seconds(0.4), seconds(0.5)]);
+legend("V_{in}", "V_{LDV}");
