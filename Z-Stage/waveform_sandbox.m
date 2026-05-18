7e47f0c4bc93be139b0b085f2c6f8419196d9caa
Assenov <ioancho@UMICH.EDU>
2026-05-18 15:20:28 -0400

    Add waveshape setting and saving functionality.

--- Z-Stage/waveform_sandbox.m
+++ Z-Stage/waveform_sandbox.m
@@ -8,17 +8,17 @@ clear; clc;
 % Using device #35
 %
 % Sustained high-displacement nonlinear response with following parameters:
-% Duration: 15, downsweep time: 1.49, Freq high: 3000, Freq low: 2740
+% SAWTOOTH, Duration: 15, downsweep time: 1.49, Freq high: 3000, Freq low: 2740
 % Using device #35
 %
 % Sustained high-displacement linear response w/ params:
-% Duration: 10, downsweep time: 0, Freq high: 3000, Freq low 1070
+% SQUARE, Duration: 10, downsweep time: 0, Freq high: 3000, Freq low 1070
 % Using device #35
 % ------------------------------ Bookmarks --------------------------------
 
-% Adjustable downsweep parameters
-fl = 2740; % lowest freq (Hold at this frequency) symbol: f_l
-fh = 3000; % highest freq symbol: f_h
+% Adjustable downsweep parameters. If fh is lower than fl it becomes upsweep.
+fh = 3000; % highest freq (Start at this frequency)
+fl = 2740; % lowest freq (End at this frequency and maintain)
 
 % Define downsweep profile via sweeprate
 % sweepRate = 25; % [Hz/s]
@@ -30,11 +30,13 @@ sweepRate = (fh-fl)/downsweepTime;
 
 duration = 10; % [s]
 
+waveshape = "SQUARE"; % Can be SQUARE, SINE, or SAWTOOTH
+
 % Create piecewise signal.
 % Sweep down from higher frequency into the frequency we want to hold for
 % testing. Downsweep helps mirror move and achieve higher displacements.
 fs = 100000; % Sampling frequency (DAQ Rate)
-amplitude = 25/6; % 1.25 = 50, 1.5 = 60, 1.75 = 70, 2 = 80
+amplitude = 25/6; % One side driving voltage divided by the gain.
 tDownsweep = linspace(0, downsweepTime, fs*downsweepTime);
 tHold = linspace(downsweepTime, duration, fs*(duration-downsweepTime));
 t = linspace(0,duration,fs*duration); % t = [tRampup, tHold]; or just total time
@@ -43,14 +45,25 @@ t = linspace(0,duration,fs*duration); % t = [tRampup, tHold]; or just total time
 rampupSignal = linspace(fh, fl, length(tDownsweep));
 holdSignal = linspace(fl, fl, length(tHold));
 fullSignal = [rampupSignal holdSignal];
-% downsweep = sin(2*pi*cumsum(fullSignal)/fs); % Sinus wave
-% downsweep = duty_cycle(downsweep', 0.5, amplitude); % Square wave (sinus wave definition required).
-downsweep =  amplitude*sawtooth(2*pi*cumsum(fullSignal)/fs, 1); % Sawtooth wave downsweep from high to low freq and hold low.
+
+if strcmp(waveshape, "SINE")
+    fprintf("Sine waveform selected.\n");
+    downsweep = amplitude*sin(2*pi*cumsum(fullSignal)/fs); % Sine wave
+elseif strcmp(waveshape, "SQUARE")
+    fprintf("Square waveform selected.\n");
+    downsweep = sin(2*pi*cumsum(fullSignal)/fs); % Sinus wave
+    downsweep = duty_cycle(downsweep', 0.5, amplitude); % Square wave, 50% duty cycle (sinus wave definition required).
+elseif strcmp(waveshape, "SAWTOOTH")
+    fprintf("Sawtooth waveform selected.\n");
+    downsweep =  amplitude*sawtooth(2*pi*cumsum(fullSignal)/fs, 1); % Sawtooth wave downsweep from high to low freq and hold low.
+else
+    error("Invalid waveform selected");
+end
 
 % initialize
 daqreset;
-d = daqlist("ni") %#ok<*NOPTS>
-deviceinfo1 = d{1,"DeviceInfo"}
+d = daqlist("ni"); %#ok<*NOPTS>
+deviceinfo1 = d{1,"DeviceInfo"};
 dq = daq("ni"); %init
 dq.Rate = fs; % Daq rate
 
@@ -60,6 +73,7 @@ addinput(dq,'cDAQ9185-1C61526Mod1','ai1',"Voltage"); % Output voltage (from LDV)
 addoutput(dq,'cDAQ9185-1C61526Mod2',"ao0","Voltage"); % Input voltage (into mirror)
 
 % simultaneous acquiring and writing data
+fprintf("Running experiment.\n");
 data2 = readwrite(dq, downsweep'); % IF SQUARE WAVE ADD AN '
 data2 = renamevars(data2,["cDAQ9185-1C61526Mod1_ai0","cDAQ9185-1C61526Mod1_ai1"], ["V_in","V_LDV"]);
 
@@ -75,8 +89,9 @@ z_down = lsim(Gint*GHPF*GLPF, dzdtfilt, t);
 
 % Plotting
 % close all;
-paramString = sprintf("Duration: %d, Downsweep time: %d, f_h: %d, f_l: %d", duration, downsweepTime, fl, fh); % Define parameter string to encode unique experiment parameters (for reproducibility)
+paramString = sprintf("Duration: %d, Downsweep time: %d, f_h: %d, f_l: %d", duration, downsweepTime, fh, fl); % Define parameter string to encode unique experiment parameters (for reproducibility)
 
+fprintf("Displaying plot.\n");
 
 figure();
 subplot(2, 1, 1);
@@ -102,3 +117,11 @@ function outputArray = duty_cycle(array, d, a)
         end
     end
 end
+
+fprintf("Program completed.\n");
+% Save data file
+% Save time, displacement-frequency plot, and displacement-time plot.
+filename = sprintf("%s,duration-%d,sweeptime-%d,f_h-%d,f_l-%d", waveshape, duration, downsweepTime, fh, fl);
+filename = filename + ".mat";
+save("../Data/"+filename, "t", "fTimeDown", "z_down");
+fprintf("Saved %s to Data/\n", filename);
